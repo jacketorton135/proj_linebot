cedb7548ed78de9752654d648b9930879facb2fd
danny900618 <c0981985611@gmail.com>
2024-02-18 12:24:37 +0800

    deploy to render

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+from thingspeak import Thingspeak
+import matplotlib.pyplot as plt
+import numpy as np
+
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('WrWuc3sqv+gIpcqpUD23unz6vy3Zq6tUg1z2vO8Mp4iWHkZCbIcJ/XnsbsXpgmfALS6VNNAGhTydI26i3dE20iHKhRxaJdSieC6V579F/onwkagqVGyp9BWwiJK3ua3vwefdmCwO95N4WXORXk6ebQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('zesmpgsWsUy3JJtv+giJb/4cDV3L3g3JGrSodEArwQwpHJadCUTrhk6EEfQ5WzjImdeR4EWvMrzi+VQVvyY2oE9pOJUuTGYiXGdh06vPgn7tp3OSZ1asIvaSURETV+u6f8OheWISM9V32C6wxwj7YgdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('b341b992fd91941e3662f525178b01f9')
+handler = WebhookHandler('1b973f1211a9861d3a71bb6674dfc1bb')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -30,13 +34,34 @@ def callback():
         abort(400)
     return 'OK'
 
+# input:  "圖表:2374700,2KNDBSF9FN4M5EY1"
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    user_msg = event.message.text
+    check = user_msg[:2].lower()
+    data = user_msg[3:]  # 2374700,2KNDBSF9FN4M5EY1
+    x = np.linspace(0.0, 2*np.pi)
 
-import os
+    if check in "圖表:":
+        channel_id = data.split(',')[0]
+        key = data.split(',')[1]
+        ts = Thingspeak()
+        tw_time_list, bpm_list = ts.get_data_from_thingspeak(channel_id, key)
+        ts.gen_chart(tw_time_list, bpm_list)
+        chart_link = ts.upload_to_imgur()
+        image_message = ImageSendMessage(
+            original_content_url=chart_link,
+            preview_image_url=chart_link
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+
+        message = TextSendMessage(text=event.message.text)
+        line_bot_api.reply_message(event.reply_token, message)
+        # chart = Thingspeak().get_data_from_thingspeak()
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5001))
+    # app.run(host='0.0.0.0', port=5001)
+    app.run()
---
+++ thingspeak.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime, timedelta
+import pytz
+	
+class Thingspeak():
+    def get_data_from_thingspeak(self, channel_id, api_read_key):
+
+        url = 'https://thingspeak.com/channels/{channel_id}/feed.json?api_key={api_read_key}'.format(channel_id = channel_id,api_read_key = api_read_key)
+        data = requests.get(url).json()
+        time_list = list()
+        entry_id_list = list()
+        bpm_list = list()
+        for data in data['feeds']:
+            time_list.append(data.get('created_at'))
+            entry_id_list.append(data.get('entry_id'))
+            if data.get('field1') == '0':
+                bpm_list.append('0.01')
+            else:
+                bpm_list.append(data.get('field1'))
+
+        #換成台灣時間
+        # print(time_list)
+        tw_time_list = self.format_time(time_list)
+        # print(tw_time_list)
+        return tw_time_list, bpm_list
+    def format_time(self, time_list):
+        # 解析时间字符串并转换为台湾时间
+        taiwan_tz = pytz.timezone('Asia/Taipei')
+        tw_time_list = []
+        for timestamp in time_list:
+            dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+            dt_utc = pytz.utc.localize(dt)
+            dt_taiwan = dt_utc.astimezone(taiwan_tz)
+            tw_time_list.append(dt_taiwan.strftime('%Y-%m-%d %H:%M:%S'))
+        return tw_time_list
+    
+    # 從 JSON 數據中提取數字並繪製折線圖
+    def gen_chart(self, time_list, bpm_list):
+        print(time_list, bpm_list)
+        plt.figure(figsize=(12, 15))  # 設置圖片尺寸為 10x6
+        bpm_list = [float(value) for value in bpm_list]
+        # 绘制图表
+        plt.plot(time_list, bpm_list, 'r-o')
+        plt.xlabel('Time')
+        plt.ylabel('BPM')
+        plt.title('Thingspeak')
+        plt.xticks(rotation=45)
+        plt.savefig('chart.png', format='png')
+        return 
+    
+    # 上傳圖片到 Imgur
+    def upload_to_imgur(self):
+        # 上傳圖片到 Imgur
+        client_id = '1057e1ccf4ca17c'  # 替換成你的 Imgur Client ID
+
+        headers = {'Authorization': 'Client-ID {}'.format(client_id)}
+        data = {'image': open('chart.png', 'rb').read()}
+        response = requests.post('https://api.imgur.com/3/image', headers=headers, data=data)
+        if response.status_code == 200:
+            return response.json()['data']['link']
+        else:
+            print('Error uploading image to Imgur')
+            return None
+        
+if __name__ == "__main__":
+    ts = Thingspeak()
+    ts.get_data_from_thingspeak()
