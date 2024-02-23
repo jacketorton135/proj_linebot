f6ff4ee551f205726e00ae6328da2af0d2a11bd1
danny.chao <danny.chao@bionime.com>
2024-02-23 08:59:20 +0800

    upload update

--- thingspeak.py
+++ thingspeak.py
@@ -5,6 +5,8 @@ import numpy as np
 from datetime import datetime, timedelta
 import pytz
 import pyimgur
+from imgurpython import ImgurClient
+
 
 class Thingspeak():
     def get_data_from_thingspeak(self, channel_id, api_read_key):
@@ -52,15 +54,33 @@ class Thingspeak():
 
     # 上傳圖片到 Imgur
     def upload_to_imgur(self):
-        CLIENT_ID = "1057e1ccf4ca17c"
-        PATH = "chart.png" #A Filepath to an image on your computer"
-        title = "Uploaded with PyImgur"
+        client_id = '1057e1ccf4ca17c'
+        client_secret = '4207a25500bd516266c39c3f831ab8da7d47d0a1'
 
-        im = pyimgur.Imgur(CLIENT_ID)
-        uploaded_image = im.upload_image(PATH, title=title)
-        # print(uploaded_image.title)
-        image_url = uploaded_image.link
-        return  image_url
+        client = ImgurClient(client_id, client_secret)
+
+        # Example request
+        items = client.gallery()
+        for item in items:
+            print(item.link)
+        # Authorization flow, pin example (see docs for other auth types)
+        authorization_url = client.get_auth_url('pin')
+
+        # ... redirect user to `authorization_url`, obtain pin (or code or token) ...
+
+        credentials = client.authorize(authorization_url, 'pin')
+        client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
+
+        return item.link
+        # CLIENT_ID = "1057e1ccf4ca17c"
+        # PATH = "chart.png" #A Filepath to an image on your computer"
+        # title = "Uploaded with PyImgur"
+
+        # im = pyimgur.Imgur(CLIENT_ID)
+        # uploaded_image = im.upload_image(PATH, title=title)
+        # # print(uploaded_image.title)
+        # image_url = uploaded_image.link
+        # return  image_url
         
 if __name__ == "__main__":
     ts = Thingspeak()
