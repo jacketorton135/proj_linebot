1441d5a8945c5ae17fcf755df7c06c1a6910bd5d
danny.chao <danny.chao@bionime.com>
2024-03-29 15:53:29 +0800

    bug fix

--- app.py
+++ app.py
@@ -54,58 +54,60 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print('event.message', event.message)
     get_request_user_id = event.source.user_id
-    print(get_request_user_id)
-    # get_request_user_id = line_bot_api.get_profile('user_id')
+    print('get_request_user_id', get_request_user_id)
     input_msg = event.message.text
     check = input_msg[:3].lower()
     user_msg = input_msg[3:]  # 2374700,2KNDBSF9FN4M5EY1
     print('check', check)
     print('user_msg', user_msg)
-    if get_request_user_id not in auth_user_list:
-        message = TextSendMessage(text='使用者沒有權限')
-        line_bot_api.reply_message(event.reply_token, message)
-    if check in "圖表:":
-        channel_id = user_msg.split(',')[0]
-        key = user_msg.split(',')[1]
-        print("User channel_id: ", channel_id, "Read_key: ", key)
-        ts = Thingspeak()
-        tw_time_list, bpm_list = ts.get_data_from_thingspeak(channel_id, key)
-        if tw_time_list == 'Not Found' or bpm_list == 'Not Found':
-            message = TextSendMessage(text="User not found")
+    if get_request_user_id in auth_user_list:
+
+        if check in "圖表:":
+            channel_id = user_msg.split(',')[0]
+            key = user_msg.split(',')[1]
+            print("User channel_id: ", channel_id, "Read_key: ", key)
+            ts = Thingspeak()
+            tw_time_list, bpm_list = ts.get_data_from_thingspeak(
+                channel_id, key)
+            if tw_time_list == 'Not Found' or bpm_list == 'Not Found':
+                message = TextSendMessage(text="User not found")
+                line_bot_api.reply_message(event.reply_token, message)
+            else:
+                ts.gen_chart(tw_time_list, bpm_list)
+                ts.update_photo_size()
+                chart_link, pre_chart_link = ts.upload_to_imgur()
+                print("圖片網址", chart_link)
+                print("縮圖網址", pre_chart_link)
+                image_message = ImageSendMessage(
+                    original_content_url=chart_link,
+                    preview_image_url=pre_chart_link)
+                line_bot_api.reply_message(event.reply_token, image_message)
+        elif check == 'ai:' and get_request_user_id in auth_user_ai_list:
+            client = OpenAI(api_key=openai_api_key)
+
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "如果回答問題盡可能用簡潔的話回復"
+                    },
+                    {
+                        "role": "user",
+                        "content": user_msg,
+                    },
+                ],
+            )
+            reply_msg = completion.choices[0].message.content
+            line_bot_api.reply_message(event.reply_token, reply_msg)
+
+        else:  # 學使用者說話
+
+            message = TextSendMessage(text=event.message.text)
             line_bot_api.reply_message(event.reply_token, message)
-        else:
-            ts.gen_chart(tw_time_list, bpm_list)
-            ts.update_photo_size()
-            chart_link, pre_chart_link = ts.upload_to_imgur()
-            print("圖片網址", chart_link)
-            print("縮圖網址", pre_chart_link)
-            image_message = ImageSendMessage(original_content_url=chart_link,
-                                             preview_image_url=pre_chart_link)
-            line_bot_api.reply_message(event.reply_token, image_message)
-    elif check == 'ai:' and get_request_user_id in auth_user_ai_list:
-        client = OpenAI(api_key=openai_api_key)
-
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "如果回答問題盡可能用簡潔的話回復"
-                },
-                {
-                    "role": "user",
-                    "content": user_msg,
-                },
-            ],
-        )
-        reply_msg = completion.choices[0].message.content
-        line_bot_api.reply_message(event.reply_token, reply_msg)
-
-    else:  # 學使用者說話
-
-        message = TextSendMessage(text=event.message.text)
+    else:
+        message = TextSendMessage(text='使用者沒有權限')
         line_bot_api.reply_message(event.reply_token, message)
 
 
