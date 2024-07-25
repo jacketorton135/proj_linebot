37a80ca0e875e9021d4a6be4f9f36325e0767e4d
jacketorton135 <ortonhalo@gmail.com>
2024-07-26 00:14:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
+import boto3
 import json
 from openai import OpenAI
 from flask import Flask, request, abort
 import os
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+from io import BytesIO
 from thingspeak import Thingspeak
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
+
+# 確保環境變數正確設置
 line_bot_api_key = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_secret_key = os.environ.get('LINE_CHANNEL_SECRET_KEY')
+openai_api_key = os.environ.get('OPEN_AI_KEY')
+imgur_client_id = os.environ.get('IMGUR_CLIENT_ID')
+
+# 檢查環境變數是否正確設置
+if not line_bot_api_key or not line_bot_secret_key:
+    raise EnvironmentError("LINE_CHANNEL_ACCESS_TOKEN or LINE_CHANNEL_SECRET_KEY not set")
+
+if not openai_api_key:
+    raise EnvironmentError("OPEN_AI_KEY not set")
+
+if not imgur_client_id:
+    raise EnvironmentError("IMGUR_CLIENT_ID not set")
 
 # Channel Access Token
 line_bot_api = LineBotApi(line_bot_api_key)
 # Channel Secret
 handler = WebhookHandler(line_bot_secret_key)
 
-# Open AI key
-openai_api_key = os.environ.get('OPEN_AI_KEY')
-
-# U5583a266be8eb6b47ad9fa7d96846c80
 # Auth User list
-auth_user_list = os.environ.get('AUTH_USER_LIST')  # string
-auth_user_list = auth_user_list.split(',')  #list
+auth_user_list = os.environ.get('AUTH_USER_LIST', '').split(',')
 print('auth_user_list', auth_user_list)
 
-# U5583a266be8eb6b47ad9fa7d96846c80
 # Auth AI User list
-auth_user_ai_list = os.environ.get('AUTH_AI_USER_LIST')  # string
-auth_user_ai_list = auth_user_ai_list.split(',')  #list
+auth_user_ai_list = os.environ.get('AUTH_AI_USER_LIST', '').split(',')
 print('auth_user_ai_list', auth_user_ai_list)
+
+# Auth AWS User list
+auth_user_aws_list = os.environ.get('AUTH_AWS_USER_LIST', '').split(',')
+print('auth_user_aws_list', auth_user_aws_list)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -46,7 +61,6 @@ def callback():
         abort(400)
     return 'OK'
 
-# input:  "圖表:2466473,field1,GROLYCVTU08JWN8Q"
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -54,24 +68,22 @@ def handle_message(event):
     print('get_request_user_id', get_request_user_id)
     input_msg = event.message.text
     check = input_msg[:3].lower()
-    user_msg = input_msg[3:]  # 2466473,field1,GROLYCVTU08JWN8Q
+    user_msg = input_msg[3:]  # 2374700,2KNDBSF9FN4M5EY1
     print('check', check)
     print('user_msg', user_msg)
     if get_request_user_id in auth_user_list:
-
-        if check in "圖表:":
+        if check == "圖表:":
             channel_id = user_msg.split(',')[0]
-            field = user_msg.split(',')[1]
-            key = user_msg.split(',')[2]
-            print("User channel_id: ", channel_id, "Field: ", field, "Read_key: ", key)
+            key = user_msg.split(',')[1]
+            field = user_msg.split(',')[2] if len(user_msg.split(',')) > 2 else 'field1'
+            print("User channel_id: ", channel_id, "Read_key: ", key, "Field: ", field)
             ts = Thingspeak()
-            tw_time_list, bpm_list = ts.get_data_from_thingspeak(
-                channel_id, field, key)
-            if tw_time_list == 'Not Found' or bpm_list == 'Not Found':
+            tw_time_list, data_list = ts.get_data_from_thingspeak(channel_id, field, key)
+            if tw_time_list == 'Not Found' or data_list == 'Not Found':
                 message = TextSendMessage(text="User not found")
                 line_bot_api.reply_message(event.reply_token, message)
             else:
-                ts.gen_chart(tw_time_list, bpm_list)
+                ts.gen_chart(tw_time_list, data_list)
                 ts.update_photo_size()
                 chart_link, pre_chart_link = ts.upload_to_imgur()
                 print("圖片網址", chart_link)
@@ -84,7 +96,7 @@ def handle_message(event):
             try:
                 client = OpenAI(api_key=openai_api_key)
 
-                response = client.chat.completions.create(
+                response = client.chat_completions.create(
                     model="gpt-3.5-turbo-0125",
                     messages=[
                         {
@@ -110,12 +122,6 @@ def handle_message(event):
         message = TextSendMessage(text='使用者沒有權限')
         line_bot_api.reply_message(event.reply_token, message)
 
-
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5001))
-    # local
-    # app.run(host='0.0.0.0', port=5001)
-    # Render
     app.run()
-
 
