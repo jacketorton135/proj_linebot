e2270a11992503d40e555992acae9dd9710c12dd
jacketorton135 <ortonhalo@gmail.com>
2024-07-25 23:52:08 +0800

    Update thingspeak.py

--- thingspeak.py
+++ thingspeak.py
@@ -1,72 +1,83 @@
-from flask import Flask, request, abort
 import os
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
-from thingspeak import Thingspeak
-from dotenv import load_dotenv
+import requests
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+import pytz
+import pyimgur
+from PIL import Image
 
-# Load environment variables from .env file
-load_dotenv()
+class Thingspeak():
+    def get_data_from_thingspeak(self, channel_id, field, api_read_key):
+        url = f'https://thingspeak.com/channels/{channel_id}/feed.json?api_key={api_read_key}'
+        data = requests.get(url).json()
+        if data.get('error') == 'Not Found':
+            return 'Not Found', 'Not Found'
+        time_list = list()
+        entry_id_list = list()
+        field_list = list()
+        for data_point in data['feeds']:
+            time_list.append(data_point.get('created_at'))
+            entry_id_list.append(data_point.get('entry_id'))
+            field_list.append(data_point.get(field))
 
-app = Flask(__name__)
-line_bot_api_key = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
-line_bot_secret_key = os.getenv('LINE_CHANNEL_SECRET_KEY')
+        # 換成台灣時間
+        tw_time_list = self.format_time(time_list)
+        return tw_time_list, field_list
+    
+    # 解析時間字符串並轉換為台灣時間
+    def format_time(self, time_list):
+        taiwan_tz = pytz.timezone('Asia/Taipei')
+        tw_time_list = []
+        for timestamp in time_list:
+            dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+            dt_utc = pytz.utc.localize(dt)
+            dt_taiwan = dt_utc.astimezone(taiwan_tz)
+            tw_time_list.append(dt_taiwan.strftime('%Y-%m-%d %H:%M:%S'))
+        return tw_time_list
+    
+    # 從 JSON 數據中提取數字並繪製折線圖
+    def gen_chart(self, time_list, field_list):
+        print(time_list, field_list)
+        plt.figure(figsize=(12, 15))  # 設置圖片尺寸為 10x6
+        field_list = [float(value) for value in field_list]
+        # 繪製圖表
+        plt.plot(time_list, field_list, 'r-o')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.title('Thingspeak')
+        plt.xticks(rotation=45)
+        plt.savefig('chart.jpg', format='jpg')
+        return 
+    
+    def update_photo_size(self):
+        img = Image.open('chart.jpg')   # 開啟圖片
+        img2 = img.resize((240,240))       # 調整圖片尺寸為 200x200
+        img2.save('pre_chart.jpg')   
 
-# Channel Access Token
-line_bot_api = LineBotApi(line_bot_api_key)
-# Channel Secret
-handler = WebhookHandler(line_bot_secret_key)
+    # 上傳圖片到 Imgur
+    def upload_to_imgur(self):
+        CLIENT_ID = os.environ.get('IMGUR_CLIENT_ID')
+        PATH = "chart.jpg"  # A Filepath to an image on your computer
+        title = "Uploaded with PyImgur"
 
-# Auth User list
-auth_user_list = os.getenv('AUTH_USER_LIST').split(',')
+        im = pyimgur.Imgur(CLIENT_ID)
+        uploaded_image = im.upload_image(PATH, title=title)
+        image_url = uploaded_image.link
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+        PATH = "pre_chart.jpg"  # A Filepath to an image on your computer
+        title = "Uploaded with pre_PyImgur"
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_request_user_id = event.source.user_id
-    input_msg = event.message.text
-    check = input_msg[:3].lower()
-    user_msg = input_msg[3:]
-
-    if get_request_user_id in auth_user_list:
-        if check == "圖表:":
-            try:
-                channel_id, field_name, key = user_msg.split(',')
-                ts = Thingspeak()
-                tw_time_list, field_data = ts.get_data_from_thingspeak(channel_id, key)
-                if tw_time_list == 'Not Found' or field_data == 'Not Found':
-                    message = TextSendMessage(text="資料不存在")
-                    line_bot_api.reply_message(event.reply_token, message)
-                else:
-                    chart_filename = ts.gen_chart(tw_time_list, field_data, field_name)
-                    image_url, pre_image_url = ts.upload_to_imgur(chart_filename)
-                    image_message = ImageSendMessage(
-                        original_content_url=image_url,
-                        preview_image_url=pre_image_url
-                    )
-                    line_bot_api.reply_message(event.reply_token, image_message)
-            except ValueError:
-                message = TextSendMessage(text="請輸入正確的格式：圖表:channel_id,field_name,key")
-                line_bot_api.reply_message(event.reply_token, message)
-        else:
-            message = TextSendMessage(text="無效的請求")
-            line_bot_api.reply_message(event.reply_token, message)
-    else:
-        message = TextSendMessage(text='使用者沒有權限')
-        line_bot_api.reply_message(event.reply_token, message)
+        pre_im = pyimgur.Imgur(CLIENT_ID)
+        uploaded_pre_image = pre_im.upload_image(PATH, title=title)
+        pre_image_url = uploaded_pre_image.link
+        return image_url, pre_image_url
 
 if __name__ == "__main__":
-    app.run()
+    ts = Thingspeak()
+    tw_time_list, field_list = ts.get_data_from_thingspeak("2466473", "field1", "GROLYCVTU08JWN8Q")
+    ts.gen_chart(tw_time_list, field_list)
+    ts.update_photo_size()
+    ts.upload_to_imgur()
 
