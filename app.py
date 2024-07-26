5a42afbadfb58d4ecab7a49cb4fadf7a0843eca1
jacketorton135 <ortonhalo@gmail.com>
2024-07-26 13:40:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 line_bot_api_key = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_secret_key = os.environ.get('LINE_CHANNEL_SECRET_KEY')
 
+# 確認環境變量是否正確設置
+if not line_bot_api_key:
+    raise ValueError("環境變量 LINE_CHANNEL_ACCESS_TOKEN 未設置")
+if not line_bot_secret_key:
+    raise ValueError("環境變量 LINE_CHANNEL_SECRET_KEY 未設置")
+
 # Channel Access Token
 line_bot_api = LineBotApi(line_bot_api_key)
 # Channel Secret
@@ -18,16 +24,20 @@ handler = WebhookHandler(line_bot_secret_key)
 
 # Open AI key
 openai_api_key = os.environ.get('OPEN_AI_KEY')
+if not openai_api_key:
+    raise ValueError("環境變量 OPEN_AI_KEY 未設置")
 
 # Auth User list
-auth_user_list = os.environ.get('AUTH_USER_LIST')  # string
-auth_user_list = auth_user_list.split(',')  # list
-print('auth_user_list', auth_user_list)
+auth_user_list = os.environ.get('AUTH_USER_LIST')
+if not auth_user_list:
+    raise ValueError("環境變量 AUTH_USER_LIST 未設置")
+auth_user_list = auth_user_list.split(',')
 
 # Auth AI User list
-auth_user_ai_list = os.environ.get('AUTH_AI_USER_LIST')  # string
-auth_user_ai_list = auth_user_ai_list.split(',')  # list
-print('auth_user_ai_list', auth_user_ai_list)
+auth_user_ai_list = os.environ.get('AUTH_USER_AI_LIST')
+if not auth_user_ai_list:
+    raise ValueError("環境變量 AUTH_USER_AI_LIST 未設置")
+auth_user_ai_list = auth_user_ai_list.split(',')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -48,17 +58,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_request_user_id = event.source.user_id
-    print('get_request_user_id', get_request_user_id)
     input_msg = event.message.text
     check = input_msg[:3].lower()
     user_msg = input_msg[3:]  # 2374700,2KNDBSF9FN4M5EY1
-    print('check', check)
-    print('user_msg', user_msg)
+
     if get_request_user_id in auth_user_list:
         if check == "圖表:":
             channel_id = user_msg.split(',')[0]
             key = user_msg.split(',')[1]
-            print("User channel_id: ", channel_id, "Read_key: ", key)
             ts = Thingspeak()
             tw_time_list, bpm_list, temperature_list, humidity_list, body_temperature_list, ECG_list = ts.get_data_from_thingspeak(
                 channel_id, key)
@@ -69,8 +76,6 @@ def handle_message(event):
                 ts.gen_chart(tw_time_list, bpm_list, temperature_list, humidity_list, body_temperature_list, ECG_list)
                 ts.update_photo_size()
                 chart_links, pre_chart_links = ts.upload_to_imgur()
-                print("圖片網址", chart_links)
-                print("縮圖網址", pre_chart_links)
                 image_messages = [ImageSendMessage(original_content_url=chart_link, preview_image_url=pre_chart_link)
                                   for chart_link, pre_chart_link in zip(chart_links, pre_chart_links)]
                 line_bot_api.reply_message(event.reply_token, image_messages)
@@ -92,7 +97,6 @@ def handle_message(event):
                     ],
                 )
                 reply_msg = response.choices[0].message.content
-                print('reply_msg', reply_msg)
                 message = TextSendMessage(text=reply_msg)
                 line_bot_api.reply_message(event.reply_token, message)
             except Exception as e:
@@ -106,4 +110,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run()
+
 
